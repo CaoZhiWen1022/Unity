39e897f49be83bf93ba01820d38d0152f4ccecb1
CaoZhiWen1022 <83941842+CaoZhiWen1022@users.noreply.github.com>
2021-05-10 01:58:41 -0500

    Add files via upload

---
+++ SceneLoading/Assets/_Scripts/SynchronousLoading.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SynchronousLoading : MonoBehaviour
+{
+    private AsyncOperation mAsyncOperation; //异步加载信息
+    public void Load(string scenename)
+    {
+        //SceneManager.LoadScene(scenename);
+        StartCoroutine(LoadSceneFunction(scenename));
+    }
+    /// <summary>
+    /// 异步加载场景
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator LoadSceneFunction(string scenename)
+    {
+        mAsyncOperation = SceneManager.LoadSceneAsync(scenename);
+        //不跳转场景，停留在当前场景
+        mAsyncOperation.allowSceneActivation = true;
+        yield return mAsyncOperation;
+    }
+
+}
